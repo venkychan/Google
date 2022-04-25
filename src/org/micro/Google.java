fa42036cdd61b0c7ff399552b569767e9c051245
venkychan <vengatajalapathy97@gmail.com>
2022-04-25 14:11:11 +0530

    new project in git

---
+++ src/org/micro/Google.java
@@ -0,0 +1,13 @@
+package org.micro;
+
+public class Google {
+	public void lang () {
+		System.out.println("hi");
+	}
+	public static void main(String[] args) {
+		Google g = new Google();
+		g.lang();
+	
+	}
+
+}
