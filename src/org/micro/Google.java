559e5c1096111752246bb7e9e97f284af3cbb43f
venkychan <vengatajalapathy97@gmail.com>
2022-04-25 15:08:02 +0530

    new correction

--- src/org/micro/Google.java
+++ src/org/micro/Google.java
@@ -4,10 +4,15 @@ public class Google {
 	public void lang () {
 		System.out.println("hi");
 	}
+	public void eng() {
+		System.out.println("tamil");
+	}
 	public static void main(String[] args) {
 		Google g = new Google();
 		g.lang();
+		g.eng();
 	
 	}
+	
 
 }
